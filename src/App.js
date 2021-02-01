400338615ba448ec33316ed71de54d6b12cb99a9
tim <timon0596@yandex.ru>
2021-02-02 01:30:24 +0300

    resolved with some defects

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import Cart from './Cart';
+import React from 'react'
+import Cart from './Cart'
 import Registration from './Registration'
 import './App.css'
 import axios from 'axios'
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from 'react-paginate'
 import Search from './Search'
 import ProductFilter from './Filter/ProductFilter'
 import ProductItem from './ProductItem'
 export default class App extends React.Component {
-
-
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       modal: false,
       modalReg: false,
@@ -24,63 +22,46 @@ export default class App extends React.Component {
       perPage: 10,
       currentPage: 0,
       postData: [],
-
+      slice: [],
       checkChecked: [],
-      arrayRef: []
-
+      arrayRef: [],
     }
-
-    this.deleteHandler = this.deleteHandler.bind(this)
-    // this.handlePageClick = this.handlePageClick.bind(this);
-
   }
 
-
-
   handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    const offset = selectedPage * this.state.perPage;
+    const selectedPage = e.selected
+    const offset = selectedPage * this.state.perPage
 
-    this.setState({
-      currentPage: selectedPage,
-      offset: offset
-    }, () => {
-      this.receivedData()
-    });
-  };
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset,
+      },
+      () => {
+        this.receivedData()
+      }
+    )
+  }
 
   receivedData() {
-    axios
-      .get(`https://api.punkapi.com/v2/beers`)
-      .then(res => {
+    axios.get(`https://api.punkapi.com/v2/beers`).then((res) => {
+      const data = res.data
+      const slice = data.slice(
+        this.state.offset,
+        this.state.offset + this.state.perPage
+      )
 
-        const data = res.data;
-        const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+      const arrayRef = Array.from({
+        length: slice.length,
+      }).map(() => React.createRef())
 
-
-        const arrayRef = Array.from({
-          length: slice.length
-        })
-          .map(() => React.createRef())
-
-
-        const postData = slice.map((pd, index) =>
-
-          React.forwardRef((props, ref) => <ProductItem
-            innerRef={this.state.arrayRef[index]}
-            key={index}
-            pd={pd}
-            addCart={this.addCart}
-          />
-          ))
-
-        this.setState({
-          pageCount: Math.ceil(data.length / this.state.perPage),
-          itog: data,
-          postData,
-          arrayRef
-        })
+      this.setState({
+        pageCount: Math.ceil(data.length / this.state.perPage),
+        itog: data,
+        arrayRef,
+        slice,
       })
+    })
   }
 
   componentDidMount() {
@@ -88,30 +69,29 @@ export default class App extends React.Component {
   }
   //ДОБАВЛЯЮ В КОРЗИНУ!!!!!!!!!!!!!!
   addCart = (Name, e, index) => {
-    // console.log('СОСТОЯНИЕ ЧЕКБОКСА', e.target.getAttribute('data'));
-    // console.log('СОСТОЯНИЕ ЧЕКБОКСА', e.target.getAttribute('data'));
     if (e.target.checked) {
       // console.log(Name);
       const Product = this.state.itog.find((elem) => {
         return elem.name === Name
-      });
+      })
 
       //!!
       // const x = e.target.getAttribute('data')
-      const { id, name, image_url } = Product;
+      const { id, name, image_url } = Product
       const newArr = [id, name, image_url]
 
-      this.setState({
-        cart: this.state.cart.concat([newArr]),
-        isEmpty: 'false'
-      }, this.counterHandler);
+      this.setState(
+        {
+          cart: this.state.cart.concat([newArr]),
+          isEmpty: 'false',
+        },
+        this.counterHandler
+      )
     } else {
-      console.log('Удаляю');
+      console.log('Удаляю')
       // console.log(this.state.cart.splice(index, 1));
 
       this.deleteHandler(index[0], e)
-
-
 
       // this.state.cart.splice(index[0], 1);
       // this.setState(
@@ -119,44 +99,31 @@ export default class App extends React.Component {
       //   this.counterHandler
       // );
     }
-
   }
 
   counterHandler = () => {
     // console.log('ТОТАЛ', this.state.total)
     this.setState({
-
       total: this.state.cart.reduce((acc, currentValue) => {
         // console.log('ACC', acc)
         // console.log('currentValue', currentValue)
         return Number(acc) + Number(currentValue[0])
-      }, 0)
+      }, 0),
     })
     // }, () => { console.log(this.state.total) })
-
   }
 
-  deleteHandler(index, x) {
-    // console.log('xxxxxxxx', x);
-    // console.log(e);
-    // if (e) {
-    //   e.target.checked = false
-    // }
-
-
-
-
-    // console.log('delete', index);
+  deleteHandler = (index, id) => {
+    const refIndex = this.state.slice.findIndex((el) => el.id === id)
+    this.state.arrayRef[refIndex].current.checked = false
     this.state.cart.splice(index, 1)
-
     this.setState(
-      (prevState) => ({ cart: [...prevState.cart] }),
-      // () => { console.log(this.state.total) }
+      (prevState) => ({
+        cart: [...prevState.cart],
+      }),
       this.counterHandler
     )
-    console.log('Новое', this.state.cart);
   }
-
 
   // getRef = (node) => { this.el = node }
 
@@ -165,44 +132,60 @@ export default class App extends React.Component {
       <>
         <h1>Каталог товаров</h1>
 
-        <div className="row">
-          {this.state.postData}
+        <div className='row'>
+          {this.state.slice.map((pd, index) => (
+            <ProductItem
+              pd={pd}
+              key={index}
+              ref={this.state.arrayRef[index]}
+              addCart={this.addCart}
+            />
+          ))}
         </div>
 
-
-        <div className="row">
-          {/* {this.state.itog} */}
-        </div>
+        <div className='row'>{/* {this.state.itog} */}</div>
 
         <ReactPaginate
-          previousLabel={"prev"}
-          nextLabel={"next"}
-          breakLabel={"..."}
-          breakClassName={"break-me"}
+          previousLabel={'prev'}
+          nextLabel={'next'}
+          breakLabel={'...'}
+          breakClassName={'break-me'}
           pageCount={this.state.pageCount}
           marginPagesDisplayed={2}
           pageRangeDisplayed={5}
           onPageChange={this.handlePageClick}
-          containerClassName={"pagination"}
-          subContainerClassName={"pages pagination"}
-          activeClassName={"active"} />
+          containerClassName={'pagination'}
+          subContainerClassName={'pages pagination'}
+          activeClassName={'active'}
+        />
 
-        { this.state.modal ? <Cart
-          close={() => this.setState({ modal: false })}
-          cart={this.state.cart}
-          isEmpty={this.state.isEmpty}
-          total={this.state.total}
-          deleteHandler={this.deleteHandler}
-        /> : null}
+        {this.state.modal ? (
+          <Cart
+            close={() => this.setState({ modal: false })}
+            cart={this.state.cart}
+            isEmpty={this.state.isEmpty}
+            total={this.state.total}
+            deleteHandler={this.deleteHandler}
+          />
+        ) : null}
 
-        { this.state.modalReg ? <Registration close={() => this.setState({ modalReg: false })} /> : null}
+        {this.state.modalReg ? (
+          <Registration close={() => this.setState({ modalReg: false })} />
+        ) : null}
 
-        <button className="btn green" onClick={
-          () => { this.setState({ modal: true }) }}>
+        <button
+          className='btn green'
+          onClick={() => {
+            this.setState({ modal: true })
+          }}>
           Корзина
-          </button>
+        </button>
 
-        <button className="btn blue" onClick={() => this.setState({ modalReg: true })}>Регистрация</button>
+        <button
+          className='btn blue'
+          onClick={() => this.setState({ modalReg: true })}>
+          Регистрация
+        </button>
 
         <hr />
 
@@ -213,10 +196,7 @@ export default class App extends React.Component {
           />
         </div> */}
 
-        <ProductFilter
-          data={this.state.itog}
-        />
-
+        <ProductFilter data={this.state.itog} />
       </>
     )
   }
--- src/ProductItem.js
+++ src/ProductItem.js
@@ -1,39 +1,32 @@
+import React from 'react'
 
-import React, { Component } from "react"
-
-
-class ProducItem extends Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-
-      }
-      // this.handleChange = this.handleChange.bind(this)
-
-      this.myRef = React.createRef();
-   }
-
-   render() {
-      return (
-         <div className="col s3 offset-s1">
-            <div className="card">
-               <div className="card-image">
-                  <img src={this.props.pd.image_url} />
-                  <span className="card-title red-text">{this.props.pd.name}</span>
-               </div>
-               <div className="card-content">
-                  <p>99 $</p>
-               </div>
-               <div className="card-action">
-                  <label>
-                     <input ref={this.props.innerRef} type="checkbox" onChange={(e) => this.props.addCart(this.props.pd.name, e, this.props.pd.id)} />
-                     <span>Add to cart</span>
-                  </label>
-               </div>
-            </div>
-         </div>
-      )
-   }
+const ProducItem = (props) => {
+  const { pd, innerRef, addCart } = props
+  return (
+    <div className='col s3 offset-s1'>
+      <div className='card'>
+        <div className='card-image'>
+          <img src={pd.image_url} />
+          <span className='card-title red-text'>{pd.name}</span>
+        </div>
+        <div className='card-content'>
+          <p>99 $</p>
+        </div>
+        <div className='card-action'>
+          <label>
+            <input
+              ref={innerRef}
+              type='checkbox'
+              onChange={(e) => addCart(pd.name, e, pd.id)}
+            />
+            <span>Add to cart</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default ProducItem
+export default React.forwardRef((props, ref) => (
+  <ProducItem innerRef={ref} {...props} />
+))
